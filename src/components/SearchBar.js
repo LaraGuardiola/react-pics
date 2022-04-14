68ed04cac2e7874c4b036045b238828dfef9d67f
Sergio <webdev.slara@gmail.com>
2022-04-14 18:04:32 +0200

    getting input from the user

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 class SearchBar extends React.Component{
+
+    onInputChange(event){
+        console.log(event.target.value)
+    }
+
+    //by removing the () assigned on the onChange prop in the input only a reference is passed and it's not called every time is rendered
     render(){
         return (
-            <div>
-                <form>
-                    <input type="text"></input>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image search:</label>
+                        <input type="text" onChange={this.onInputChange}/>
+                    </div>
                 </form>
             </div>
         )
